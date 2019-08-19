edd2b889ca1ce12d9c1162ab6f8c34f82e8504b6
Khlopiachyi Pavlo <khlpavlo@gmail.com>
2019-08-19 13:28:12 +0300

    ex08 done

---
+++ ex08-blockchain/blockchain.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (block *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(block.Timestamp, 10))
+	headers := bytes.Join([][]byte{block.PrevBlockHash, block.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	block.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+
+	return block
+}
+
+func (blkch *Blockchain) AddBlock(data string) {
+	prevBlock := blkch.blocks[len(blkch.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	blkch.blocks = append(blkch.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func main() {
+	blkch := NewBlockchain()
+
+	blkch.AddBlock("First")
+	blkch.AddBlock("Second")
+
+	for _, block := range blkch.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
