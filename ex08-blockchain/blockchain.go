f1e7aa8f49be692dc0cc6a4f6ecbb8ba4a55a687
Khlopiachyi Pavlo <khlpavlo@gmail.com>
2019-08-20 16:09:15 +0300

    test version

--- ex08-blockchain/blockchain.go
+++ ex08-blockchain/blockchain.go
@@ -3,19 +3,21 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/json"
-	"io"
+	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Block struct {
-	Timestamp     int64  `json: "timestamp"`
-	Data          []byte `json: "data"`
-	PrevBlockHash []byte `json: "prevblockhash"`
-	Hash          []byte `json: "hash"`
+	Timestamp     int64  `json:"timestamp"`
+	Data          []byte `json:"data"`
+	PrevBlockHash []byte `json:"prevblockhash"`
+	Hash          []byte `json:"hash"`
 }
 
 type Blockchain struct {
@@ -36,10 +38,10 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	return block
 }
 
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+func (bc *Blockchain) AddBlock(data string) *Block {
+	newBlk := NewBlock(data, []byte{})
+	bc.blocks = append(bc.blocks, newBlk)
+	return newBlk
 }
 
 func NewGenesisBlock() *Block {
@@ -50,30 +52,104 @@ func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
-func fCreate() {
-	blkch := NewBlockchain()
+func NewDb(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS blockchain(id integer PRIMARY KEY,	timestamp integer NOT NULL,	data text NOT NULL, hash text NOT NULL, prevBlockHash text NOT NULL)")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	blkch.AddBlock("First")
-	blkch.AddBlock("Second")
-	var buf = new(bytes.Buffer)
-	f, err := os.Create("hash.db.json")
-	for _, block := range blkch.blocks {
+	rows, err := db.Query("SELECT data FROM blockchain WHERE id = 1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
 
-		enc := json.NewEncoder(buf)
-		enc.Encode(block)
-
-		if nil != err {
-			log.Fatalln(err)
-		}
-		defer f.Close()
-		io.Copy(f, buf)
+	if !rows.Next() {
+		bc := NewGenesisBlock()
+		NewItem(db, bc)
 	}
 }
 
-func IsExist(fname string) {
-	if _, err := os.Open(filename); err == nil {
-		return false
+func NewItem(db *sql.DB, bc *Block) {
+	_, err := db.Exec("INSERT INTO BLOCKCHAIN (timestamp, data, hash, prevBlockHash) VALUES ($1, $2, $3, $4)",
+		bc.Timestamp, bc.Data, bc.Hash, bc.PrevBlockHash)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func lastItem(db *sql.DB) []byte {
+	var id int
+
+	rows, err := db.Query("SELECT * FROM blockchain WHERE ID=(SELECT MAX(id) FROM blockchain)")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+	rows.Next()
+	p := Block{}
+	er := rows.Scan(&id, &p.Timestamp, &p.Data, &p.Hash, &p.PrevBlockHash)
+	if er != nil {
+		log.Fatalln(er)
+	}
+	return p.Hash
+}
+
+func ShowList(db *sql.DB) {
+	var id int
+
+	rows, err := db.Query("SELECT * FROM blockchain")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	return true
+	defer rows.Close()
+
+	for rows.Next() {
+		p := Block{}
+		err := rows.Scan(&id, &p.Timestamp, &p.Data, &p.Hash, &p.PrevBlockHash)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Printf("Prev Hash: %x\n", p.PrevBlockHash)
+		fmt.Printf("Data: %s\n", p.Data)
+		fmt.Printf("Hash: %x\n", p.Hash)
+		fmt.Println()
+	}
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "blockchain.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	NewDb(db)
+	bc := NewBlockchain()
+	if len(os.Args) < 2 {
+		fmt.Printf("Error: not enought arguments")
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Printf("Error: not enought arguments")
+		} else {
+			lastHash := lastItem(db)
+			bl := bc.AddBlock(os.Args[2])
+			bl.PrevBlockHash = lastHash
+			NewItem(db, bl)
+		}
+		os.Exit(0)
+	case "list":
+		ShowList(db)
+		os.Exit(0)
+	default:
+		fmt.Printf("Invalid using!!!")
+		os.Exit(0)
+	}
 }
